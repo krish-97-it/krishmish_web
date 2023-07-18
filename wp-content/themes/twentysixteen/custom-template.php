1d196b2675eec9fcd3105389ec5f341149ce75c9
Krishnendu Nandy <krishnendunandy6@gmail.com>
2023-07-18 19:50:42 +0530

    branch-1 #comment wordpress project initial code push

---
+++ wp-content/themes/twentysixteen/custom-template.php
@@ -0,0 +1,17 @@
+<?php
+    /*
+     Template Name: custom template
+     Author: Krishnendu
+    */
+    get_header('custom');
+?>
+<section>
+    <div class="container">
+        <div class="row">
+            <h2>Hi everyone,</h2>
+            <p>I am Krishnendu Nandy from Kolkata West Bengal. I am working as a Software Developer, basically create dynamic features in Websites and and Api for data manupulation in db. i also work on automation of a existing feature. I am comfortable with both frontend and backend.</p>
+        </div>
+    </div>
+</section>
+<?php get_footer('custom');
+?>
